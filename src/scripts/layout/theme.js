52d477a058e75cf077febe1138da2c34ae4697a7
Ankita <ankita@ajency.in>
2021-02-22 14:35:12 +0530

    issue fixes

--- src/scripts/layout/theme.js
+++ src/scripts/layout/theme.js
@@ -24,38 +24,42 @@ cookieTest('no-cookies', 'cookies');
 responsiveVideos();
 responsiveTables();
 
-
 $(document).ready(function() 
 {
-$('.hamburger').on('click', function(){
-  $('.menu-list').removeClass('d-none');
-  $('.menu-list').addClass('d-block');
-  $(".mobile-menu").css("left", "0");
-  $('.overlay').removeClass('d-none');
-  $('.overlay').addClass('d-block');
-  $('.close-icon').removeClass('d-none');
-  $('.close-icon').addClass('d-block');
-});
+  $('.hamburger').on('click', function(){
+    $('.menu-list').removeClass('d-none');
+    $('.menu-list').addClass('d-block');
+    $(".mobile-menu").css("left", "0");
+    $('.overlay').css("display", "block");
+    $('.close-icon').css("opacity", "1");
+  });
 
-$('.close').on('click', function(){
-  $('.menu-list').removeClass('d-block');
-  $('.menu-list').addClass('d-none');
-});
+  $('.close').on('click', function(){
+    $('.menu-list').removeClass('d-block');
+    $('.menu-list').addClass('d-none');
+  });
 
-$('.close-icon').on('click', function(){
-  $(".mobile-menu").css("left", "-325px");
-  $('.overlay').removeClass('d-block');
-  $('.overlay').addClass('d-none');
-  $('.close-icon').removeClass('d-block');
-  $('.close-icon').addClass('d-none');
-});
 
-$('.user').on('click', function(){
-  $('.user-account').toggleClass('d-block d-none');
-});
+  $('.close-icon').on('click', function(){
+    $(".mobile-menu").css("left", "-325px");
+    $('.overlay').css("display", "none");
+    $('.close-icon').css("opacity", "0");
+  });
 
-$(".mweb-mobile-menu li.main-child-menu").click(function(){
-    $("li.main-child-menu").not(this).removeClass("show").children("ul").hide("slow");
-    $(this).addClass("show").children("ul").show("slow");
-});
+  $('.user').on('click', function(){
+    $('.user-account').toggleClass('d-block d-none');
+  });
+
+
+  $('.mweb-mobile-menu > li').click(function(e) {
+    e.stopPropagation();
+    var $el = $('ul',this);
+    $(this).toggleClass("show");
+    $('.mweb-mobile-menu > li > ul').not($el).slideUp();
+    $el.stop(true, true).slideToggle(400);
+  });
+  $('.mweb-mobile-menu > li > ul > li').click(function(e) {
+    e.stopImmediatePropagation();  
+  });
+
 });
