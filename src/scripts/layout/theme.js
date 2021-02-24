516e2466cb3ce3f2246b5accac055896cff11faa
Ankita <ankita@ajency.in>
2021-02-24 15:10:10 +0530

    single product page

--- src/scripts/layout/theme.js
+++ src/scripts/layout/theme.js
@@ -6,7 +6,6 @@ import 'lazysizes';
 import 'lazysizes/plugins/respimg/ls.respimg';
 
 import '../../styles/theme.scss';
-import '../../styles/theme.scss.liquid';
 
 import {focusHash, bindInPageLinks} from '@shopify/theme-a11y';
 import {cookieTest} from '@elkfox/shopify-theme/scripts/cookies';
@@ -32,11 +31,13 @@ $(document).ready(function()
     $(".mobile-menu").css("left", "0");
     $('.overlay').css("display", "block");
     $('.close-icon').css("opacity", "1");
+    $('.hamburger').css("background", "#EEEEEE");
   });
 
   $('.close').on('click', function(){
     $('.menu-list').removeClass('d-block');
     $('.menu-list').addClass('d-none');
+    $('.hamburger').css("background-color", "#FFF");
   });
 
 
@@ -44,22 +45,28 @@ $(document).ready(function()
     $(".mobile-menu").css("left", "-325px");
     $('.overlay').css("display", "none");
     $('.close-icon').css("opacity", "0");
+    $('.hamburger').css("background-color", "#FFF");
   });
 
   $('.user').on('click', function(){
     $('.user-account').toggleClass('d-block d-none');
   });
 
-
+  $('.search-button').on('click', function(){
+    $('.search-popup').addClass('active');
+  });
+  $('.form-close-icon').on('click', function(){
+    $('.search-popup').removeClass('active');
+  });
   $('.mweb-mobile-menu > li').click(function(e) {
+    $(this).removeClass('show');
     e.stopPropagation();
     var $el = $('ul',this);
-    $(this).toggleClass("show");
     $('.mweb-mobile-menu > li > ul').not($el).slideUp();
     $el.stop(true, true).slideToggle(400);
-  });
-  $('.mweb-mobile-menu > li > ul > li').click(function(e) {
-    e.stopImmediatePropagation();  
-  });
-
+    $(this).toggleClass('show');
+    });
+    $('.mweb-mobile-menu > li > ul > li').click(function(e) {
+      e.stopImmediatePropagation();  
+    });
 });
